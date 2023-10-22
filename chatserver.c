54dbb46ca9c5851eb5d461f1254afa3e399c45ce
Gabe Ong <gabe.o7578@gmail.com>
2023-10-21 21:58:08 -0700

    rset

--- chatserver.c
+++ chatserver.c
@@ -6,7 +6,7 @@
 #include <arpa/inet.h>
 
 
-int mport=6005;
+int mport=6004;
 int yport=6005;
 
 void* sending(){
@@ -24,7 +24,7 @@ int main (){
 
     struct sockaddr_in myaddr;
     struct sockaddr_in youaddr;
-
+    
     if((mfd=socket(AF_INET, SOCK_DGRAM, 0))<0){
         perror("cannot create socket");
         return 0;
@@ -37,7 +37,7 @@ int main (){
 
     memset((char*)&myaddr,0,sizeof(myaddr));
     myaddr.sin_family = AF_INET; //protocall
-    myaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); //use my ip address
+    myaddr.sin_addr.s_addr = inet_addr("142.58.15.124"); //use my ip address
     myaddr.sin_port=htons(mport);//my port
     if(bind(mfd, (struct sockaddr*)&myaddr, sizeof(myaddr))<0){
         perror("bind failed");
@@ -46,7 +46,7 @@ int main (){
     memset((char*)&youaddr,0,sizeof(youaddr));
     youaddr.sin_family = AF_INET; //protocall
     youaddr.sin_port=htons(yport);//their port
-    if (inet_aton("127.0.0.1", &youaddr.sin_addr)==0) {
+    if (inet_aton("142.58.15.124", &youaddr.sin_addr)==0) {
 		fprintf(stderr, "inet_aton() failed\n");
 	}
 
@@ -54,21 +54,28 @@ int main (){
     
     int recvlen;
     for(;;){
-        printf("enter the message\n");
-        fgets(buff,1024,stdin);
+        FD_ZERO(&rset);
+        FD_SET(0,&rset);
+        FD_SET(tfd, &rset);
 
-        if(sendto(tfd, buff, 1024, 0, (struct sockaddr*)&youaddr, sizeof(youaddr))<0){
-            perror("sento failed");
-        
+        select(tfd+1,&rest, NULL,NULL );
+        if(FD_ISSET(0,&rset)){
+            printf("enter the message\n");
+            fgets(buff,1024,stdin);
+            if(sendto(tfd, buff, 1024, 0, (struct sockaddr*)&youaddr, sizeof(youaddr))<0){
+                perror("sendto failed");
+            }
         }
-        printf("waiting on port %d\n", mport);
-        recvlen=recvfrom(tfd, buff, 1024,0,(struct sockaddr*)&myaddr,sizeof(myaddr));
-        printf("received %d bytes\n", recvlen);
-        if(recvlen>0){
-            buff[recvlen]=0;
-            printf("received message: \"%s\"\n", buff);
+
+        if(FD_ISSET(tfd,&rset)){
+            printf("waiting on port %d\n", mport);
+            recvlen=recvfrom(tfd, buff, 1024,0,(struct sockaddr*)&myaddr,sizeof(myaddr));
+            printf("received %d bytes\n", recvlen);
+            if(recvlen>0){
+                buff[recvlen]=0;
+                printf("received message: \"%s\"\n", buff);
+            }
         }
-        
     }
     //sending messages
     
