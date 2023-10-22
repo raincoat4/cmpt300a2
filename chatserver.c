58b2be3401e0b9a76438c397446e3de0763201ec
Gabe Ong <gabe.o7578@gmail.com>
2023-10-21 18:23:23 -0700

    changed from TCP to UDP

--- chatserver.c
+++ chatserver.c
@@ -6,36 +6,70 @@
 #include <arpa/inet.h>
 
 int client(int sfd, fd_set rset){
-    
+    struct sockaddr_in server;
+
     for(;;){
         FD_ZERO(&rset);
         FD_SET(0,&rset);
         FD_SET(sfd, &rset);
         char buff[1024]= " ";
+
         select(sfd +1, &rset, NULL, NULL, NULL);
 
         if(FD_ISSET(0,&rset)){
             printf("Enter the message:\n");
-            fgets(buff, 1024, stdin);
+            fgets(buff, 1024, stdin);//listening for keyboard input
             //scanf("%s", buff);
-            write(sfd, buff, strlen(buff));
+            sendto(sfd, buff, strlen(buff),0, (struct sockaddr*)&server, addr_len);//writing to UDP datagram
         }
 
         if(FD_ISSET(sfd, &rset)){
-            read(sfd, buff, 1024);
-            printf("Message received: %s\n", buff);
+            recvfrom(sfd, buff, 1024,1024,0,(struct sockaddr*)&server, addr_len);//receiving datagram
+            printf("Message received: %s\n", buff);//printing message to screen
         }
     }
     close(sfd);
+
     return 0;
 
 }
+/*
+int server(int sfd, fd_set rset){
+    struct sockaddr_in server;
+
+    for(;;){
+        FD_ZERO(&rset);
+        FD_SET(0,&rset);
+        FD_SET(sfd, &rset);
+        char buff[1024]= " ";
+
+        select(sfd +1, &rset, NULL, NULL, NULL);
+
+        if(FD_ISSET(0,&rset)){
+            printf("Enter the message:\n");
+            fgets(buff, 1024, stdin);//listening for keyboard input
+            //scanf("%s", buff);
+            sendto(sfd, buff, strlen(buff),0, (struct sockaddr*)&server, addr_len);//writing to UDP datagram
+        }
+
+        if(FD_ISSET(sfd, &rset)){
+            recvfrom(sfd, buff, 1024,1024,0,(struct sockaddr*)&server, addr_len);//receiving datagram
+            printf("Message received: %s\n", buff);//printing message to screen
+        }
+    }
+    close(sfd);
+
+    return 0;
+
+}
+*/
 
 int main(){
-    int sfd, cfd; //sfd is our socket, cfd is the connection
+    int sfd; //sfd is our socket, cfd is the connection
     fd_set rset;
     
     struct sockaddr_in server;
+    socklen_t addr_len = sizeof(client);
 
     sfd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -48,45 +82,16 @@ int main(){
     server.sin_family = AF_INET;
     server.sin_port = htons(6005);
     inet_aton("142.58.15.124", &server.sin_addr);
-
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
     
 
-    printf("Attempting to connect\n");
-    if(connect(sfd, (struct sockaddr*)&server, sizeof(server))!=-1){
-        printf("Connection succeeded\n");
-        client(sfd,rset);
-        return 0;
-    }
-
-    printf("connection failed\n");
     if(bind(sfd, (struct sockaddr*)&server, sizeof(server)) < 0){
         printf("Binding failed. \n");
         return -1;
     }
 
-    listen(sfd, 7);
-    cfd = accept(sfd, NULL, NULL);
-    for (;;){
-        FD_ZERO(&rset);
-        FD_SET(0, &rset);
-        FD_SET(cfd, &rset); //cfd
-        char buff[1024] = " ";
-        select(cfd+1, &rset, NULL, NULL, NULL);
+    client(sfd, rset);
 
-        if (FD_ISSET(0, &rset)){//if rset is empty send a message
-            printf("Enter the message:\n");
-            fgets(buff, 1024, stdin);
-            //scanf("%s", buff);
-            write(cfd, buff, strlen(buff));
-        }
-
-        if(FD_ISSET(cfd, &rset)){//receiving a message
-            read(cfd, buff, 1024);
-            printf("Message received; %s\n", buff);
-        }
-    }
-    close(cfd);
-    close(sfd);
     return 0;
 }
 
